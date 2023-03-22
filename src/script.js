b83c12fd010205d5e15f6aed0b783b70216fd331
Mhiw <103310488+Mhiw@users.noreply.github.com>
2023-03-22 20:53:40 +0100

    Something is happening, not anything good though

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://cataas.com/cat");
+xhr.responseType = "arraybuffer";
+xhr.onload = function() {
+    if(xhr.status === 200) {
+        const img = document.createElement("img");
+        img.src = "data:image/jpeg:base64," + btoa(String.fromCharCode.apply(null, new Uint8Array(xhr.response)));
+        document.body.appendChild(img);
+    } else {
+        console.error("Error: ", xhr.status);
+    }
+};
+xhr.send();
